bebc11634bd5cb0b640546fd2a0af0ff4d990739
Jiajie Chen <c@jia.je>
2024-02-26 13:53:59 +0800

    feat: parallelize arch processing

--- src/bin/dickens-topic.rs
+++ src/bin/dickens-topic.rs
@@ -114,6 +114,48 @@ struct Res {
     diff: String,
 }
 
+async fn handle_arch(arch: &str, topic: String) -> anyhow::Result<Vec<Res>> {
+    let mut res = vec![];
+    let topic_pkgs = fetch_pkgs(arch, &topic).await?;
+    let stable_pkgs = fetch_pkgs(arch, "stable").await?;
+    for topic_pkg in topic_pkgs {
+        if topic_pkg.package.ends_with("-dbg") {
+            continue;
+        }
+
+        if let Some(found) = stable_pkgs.iter().find(|p| p.package == topic_pkg.package) {
+            info!(
+                "Found upgrade {} from {} to {}",
+                topic_pkg.package, found.version, topic_pkg.version
+            );
+
+            // download topic pkg
+            let left = download_pkg(&found).await?;
+            let right = download_pkg(&topic_pkg).await?;
+            let diff = Command::new("./diff-deb.sh")
+                .arg(left)
+                .arg(right)
+                .output()?;
+
+            let new_res = Res {
+                package: topic_pkg.package.clone(),
+                archs: vec![topic_pkg.architecture.clone()],
+                old_version: found.version.clone(),
+                new_version: topic_pkg.version.clone(),
+                diff: String::from_utf8_lossy(&diff.stdout).to_string(),
+            };
+
+            res.push(new_res);
+        } else {
+            info!(
+                "New package {} versioned {}",
+                topic_pkg.package, topic_pkg.package
+            );
+        }
+    }
+    Ok(res)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
@@ -129,58 +171,29 @@ async fn main() -> anyhow::Result<()> {
         "ppc64el",
         "riscv64",
     ];
-    for arch in archs {
-        let topic_pkgs = fetch_pkgs(arch, &opt.topic).await?;
-        let stable_pkgs = fetch_pkgs(arch, "stable").await?;
-        for topic_pkg in topic_pkgs {
-            if topic_pkg.package.ends_with("-dbg") {
-                continue;
+    let handles: Vec<_> = archs
+        .iter()
+        .map(|arch| tokio::task::spawn(handle_arch(arch, opt.topic.clone())))
+        .collect();
+
+    for handle in handles {
+        for new_res in handle.await?? {
+            // merge or insert
+            let mut insert = true;
+            for cur in &mut res {
+                if cur.package == new_res.package
+                    && cur.old_version == new_res.old_version
+                    && cur.new_version == new_res.new_version
+                    && cur.diff == new_res.diff
+                {
+                    cur.archs.extend(new_res.archs.clone());
+                    insert = false;
+                    break;
+                }
             }
 
-            if let Some(found) = stable_pkgs.iter().find(|p| p.package == topic_pkg.package) {
-                info!(
-                    "Found upgrade {} from {} to {}",
-                    topic_pkg.package, found.version, topic_pkg.version
-                );
-
-                // download topic pkg
-                let left = download_pkg(&found).await?;
-                let right = download_pkg(&topic_pkg).await?;
-                let diff = Command::new("./diff-deb.sh")
-                    .arg(left)
-                    .arg(right)
-                    .output()?;
-
-                let new_res = Res {
-                    package: topic_pkg.package.clone(),
-                    archs: vec![topic_pkg.architecture.clone()],
-                    old_version: found.version.clone(),
-                    new_version: topic_pkg.version.clone(),
-                    diff: String::from_utf8_lossy(&diff.stdout).to_string(),
-                };
-
-                // merge or insert
-                let mut insert = true;
-                for cur in &mut res {
-                    if cur.package == new_res.package
-                        && cur.old_version == new_res.old_version
-                        && cur.new_version == new_res.new_version
-                        && cur.diff == new_res.diff
-                    {
-                        cur.archs.push(topic_pkg.architecture.clone());
-                        insert = false;
-                        break;
-                    }
-                }
-
-                if insert {
-                    res.push(new_res);
-                }
-            } else {
-                info!(
-                    "New package {} versioned {}",
-                    topic_pkg.package, topic_pkg.package
-                );
+            if insert {
+                res.push(new_res);
             }
         }
     }
