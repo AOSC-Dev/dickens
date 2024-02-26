7cfcac43ed34e56ccaa5517aba28e82b7eb10557
Jiajie Chen <c@jia.je>
2024-02-26 11:50:48 +0800

    feat: use xz to decompress Packages.xz

--- src/bin/dickens-topic.rs
+++ src/bin/dickens-topic.rs
@@ -1,13 +1,8 @@
-use abbs_meta_apml::parse;
 use clap::Parser;
-use log::{debug, error, warn};
+use log::info;
 use reqwest::StatusCode;
-use std::{
-    collections::HashMap,
-    fs::File,
-    io::Read,
-    path::{Path, PathBuf},
-};
+use std::io::{Cursor, Read};
+use xz::read::XzDecoder;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -24,10 +19,56 @@ struct Package {
     filename: String,
 }
 
+async fn fetch_pkgs(arch: &str, topic: &str) -> anyhow::Result<Vec<Package>> {
+    let mut res = vec![];
+    let repo = "https://repo.aosc.io";
+    let client = reqwest::Client::builder().build()?;
+    let url = format!(
+        "{}/debs/dists/{}/main/binary-{}/Packages.xz",
+        repo, topic, arch
+    );
+    info!("Fetching {}", url);
+    let resp = client.get(url).send().await?;
+
+    if resp.status() == StatusCode::NOT_FOUND {
+        return Ok(res);
+    }
+
+    // xz decompress
+    let bytes = resp.bytes().await?.to_vec();
+    let mut cursor = Cursor::new(&bytes);
+    let mut decoder = XzDecoder::new(&mut cursor);
+    let mut text = String::new();
+    decoder.read_to_string(&mut text).unwrap();
+
+    for part in text.split("\n\n") {
+        if part.trim().is_empty() {
+            continue;
+        }
+
+        let mut pkg = Package::default();
+        for line in part.split("\n") {
+            if let Some(colon) = line.find(":") {
+                let key = &line[..colon];
+                let value = &line[colon + 1..].trim();
+                match key {
+                    "Package" => pkg.package = value.to_string(),
+                    "Version" => pkg.version = value.to_string(),
+                    "Architecture" => pkg.architecture = value.to_string(),
+                    "Filename" => pkg.filename = value.to_string(),
+                    _ => {}
+                }
+            }
+        }
+        res.push(pkg);
+    }
+    Ok(res)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    env_logger::init();
     let opt = Cli::parse();
-    let repo = "https://repo.aosc.io";
     let archs = [
         "all",
         "amd64",
@@ -39,36 +80,17 @@ async fn main() -> anyhow::Result<()> {
         "riscv64",
     ];
     for arch in archs {
-        let resp = reqwest::get(format!(
-            "{}/debs/dists/{}/main/binary-{}/Packages",
-            repo, opt.topic, arch
-        ))
-        .await?;
-        if resp.status() == StatusCode::NOT_FOUND {
-            continue;
-        }
-
-        let text = resp.text().await?;
-        for part in text.split("\n\n") {
-            if part.trim().is_empty() {
-                continue;
+        let topic_pkgs = fetch_pkgs(arch, &opt.topic).await?;
+        let stable_pkgs = fetch_pkgs(arch, "stable").await?;
+        for topic_pkg in topic_pkgs {
+            if let Some(found) = stable_pkgs.iter().find(|p| p.package == topic_pkg.package) {
+                println!(
+                    "Upgrade {} from {} to {}",
+                    topic_pkg.package, found.version, topic_pkg.version
+                );
+            } else {
+                println!("New {} {}", topic_pkg.package, topic_pkg.package);
             }
-
-            let mut pkg = Package::default();
-            for line in part.split("\n") {
-                if let Some(colon) = line.find(":") {
-                    let key = &line[..colon];
-                    let value = &line[colon + 1..].trim();
-                    match key {
-                        "Package" => pkg.package = value.to_string(),
-                        "Version" => pkg.version = value.to_string(),
-                        "Architecture" => pkg.architecture = value.to_string(),
-                        "Filename" => pkg.filename = value.to_string(),
-                        _ => {}
-                    }
-                }
-            }
-            println!("Pkg: {:?}", pkg);
         }
     }
     Ok(())
